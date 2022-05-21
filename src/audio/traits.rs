53ce66c698b0bfacd6883a12a36a61efa82f91e0
Zack Kollar <rallokkcaz@gmail.com>
2022-05-21 02:25:22 -0700

    :hammer: Fix auto import mistype

--- src/audio/traits.rs
+++ src/audio/traits.rs
@@ -1,11 +1,9 @@
 use std::{iter::Zip, slice::IterMut};
 
-// TODO: Come up with a clearer name for this
 pub trait FramedSampleTrait<'a> {
     fn samples(&mut self) -> IterMut<'_, f32>;
 }
 
-// TODO: Come up with a clearer name for this
 impl<'a> FramedSampleTrait<'a> for &'a mut [f32] {
     fn samples(&mut self) -> IterMut<'_, f32> {
         self.iter_mut()
